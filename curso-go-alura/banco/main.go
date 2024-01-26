d6f55618154bf7dbaf5043727f54db2138d6a6bc
Rafa <rafael.mendes@unijui.edu.br>
2024-01-26 15:58:34 -0300

    adição do método depositar

--- curso-go-alura/banco/main.go
+++ curso-go-alura/banco/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
 type ContaCorrente struct {
 	titular    string
@@ -9,13 +12,20 @@ type ContaCorrente struct {
 	saldo      float64
 }
 
-func (c *ContaCorrente) Sacar(saque float64) string {
-	podeSacar := saque <= c.saldo && saque > 0
-	if podeSacar {
+func (c *ContaCorrente) Sacar(saque float64) (float64, string) {
+	if saque <= c.saldo && saque > 0 {
 		c.saldo -= saque
-		return "Saque realizado com sucesso"
+		return c.saldo, ""
 	}
-	return "Não foi possível realizar o saque"
+	return c.saldo, "Não foi possível realizar o saque"
+}
+
+func (c *ContaCorrente) Depositar(deposito float64) (float64, string) {
+	if deposito < 0 {
+		return c.saldo, "Não foi possivel realizar o deposito"
+	}
+	c.saldo += deposito
+	return c.saldo, ""
 }
 
 func main() {
@@ -23,7 +33,15 @@ func main() {
 	contaDaSilvia.titular = "Silvia"
 	contaDaSilvia.saldo = 500
 
-	fmt.Println(contaDaSilvia.saldo)
-	fmt.Println(contaDaSilvia.Sacar(500))
-	fmt.Println(contaDaSilvia.saldo)
+	saque, err := contaDaSilvia.Sacar(500)
+	if err != "" {
+		log.Fatal(err)
+	}
+	fmt.Println(saque)
+
+	deposito, err := contaDaSilvia.Depositar(100)
+	if err != "" {
+		log.Fatal(err)
+	}
+	fmt.Println(deposito)
 }
