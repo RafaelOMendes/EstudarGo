56112fc79ca104e948099bb9f6eea8ad91532620
Rafa <rafael.mendes@unijui.edu.br>
2024-04-19 09:25:18 -0300

    teste

--- curso-go-alura/appWeb/main.go
+++ curso-go-alura/appWeb/main.go
@@ -24,7 +24,7 @@ func index(w http.ResponseWriter, r *http.Request) {
 		{"Camiseta", "Azul, bem bonita", 39, 5},
 		{"Tênis", "Confortável", 89, 3},
 		{"Fone", "Muito bom", 59, 2},
-		{"Produto novo", "Muito legal", 1.99, 1},
+		{"Produto novo", "Muito legal", 1.99, 3},
 	}
 
 	temp.ExecuteTemplate(w, "Index", produtos)
